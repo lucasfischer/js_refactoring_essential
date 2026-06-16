a1b8fcb18ce77a4b1321b33ee87e582d4f995bd5
Jason Gorman <jason.gorman@codemanship.com>
2026-06-16 11:40:14 +0100

    Added Long Method example

---
+++ long_method/src/Order.js
@@ -0,0 +1,102 @@
+export class Order {
+    #items;
+    #customer;
+
+    constructor(items, customer) {
+        this.#items = items;
+        this.#customer = customer;
+    }
+
+    summarise() {
+        // Validation
+        if (this.#items == null) {
+            throw new Error("Items cannot be null");
+        }
+        if (this.#items.length === 0) {
+            throw new Error("Order must contain items");
+        }
+
+        // Subtotal calculation
+        let subtotal = 0.0;
+        for (const item of this.#items) {
+            subtotal += item.getPrice() * item.getQuantity();
+        }
+
+        // Discount rules
+        let discount = 0.0;
+        if (this.#customer.isLoyal()) {
+            discount = subtotal * 0.10;
+        } else if (subtotal > 100) {
+            discount = subtotal * 0.05;
+        }
+
+        // Tax calculation
+        const taxableAmount = subtotal - discount;
+        const tax = taxableAmount * 0.20;
+
+        // Total calculation
+        const total = taxableAmount + tax;
+
+        return new OrderSummary(subtotal, discount, tax, total);
+    }
+}
+
+export class Customer {
+    #loyal;
+
+    constructor(loyal) {
+        this.#loyal = loyal;
+    }
+
+    isLoyal() {
+        return this.#loyal;
+    }
+}
+
+export class OrderItem {
+    #price;
+    #quantity;
+
+    constructor(price, quantity) {
+        this.#price = price;
+        this.#quantity = quantity;
+    }
+
+    getPrice() {
+        return this.#price;
+    }
+
+    getQuantity() {
+        return this.#quantity;
+    }
+}
+
+export class OrderSummary {
+    #subtotal;
+    #discount;
+    #tax;
+    #total;
+
+    constructor(subtotal, discount, tax, total) {
+        this.#subtotal = subtotal;
+        this.#discount = discount;
+        this.#tax = tax;
+        this.#total = total;
+    }
+
+    getSubtotal() {
+        return this.#subtotal;
+    }
+
+    getDiscount() {
+        return this.#discount;
+    }
+
+    getTax() {
+        return this.#tax;
+    }
+
+    getTotal() {
+        return this.#total;
+    }
+}
