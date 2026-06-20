771b67679ee0744165a0b5433a47bcb5be770ac5
Jez Higgins <jez@jezuk.co.uk>
2026-06-20 11:35:31 +0100

    extracted orderCost

--- legacy_code/test/ShippingCalculator.test.js
+++ legacy_code/test/ShippingCalculator.test.js
@@ -1,10 +1,14 @@
 import { strict as assert } from "assert";
 import {ShippingCalculator} from "../src/ShippingCalculator.js";
 
+async function orderCost(orderId) {
+    const calculator = new ShippingCalculator();
+    return await calculator.calculateShipping(1001);
+}
+
 describe("ShippingCalculator", () => {
     it("Order 1001", async () => {
-        const calculator = new ShippingCalculator();
-        const cost = await calculator.calculateShipping(1001);
+        const cost = await orderCost(1001);
 
         assert.equal(cost, 2.5);
     })
