b338eebfcf01aa696ba484fc5763e4c1b3cb4613
Jez Higgins <jez@jezuk.co.uk>
2026-06-20 10:16:22 +0100

    It works!

--- comments/src/X1.js
+++ comments/src/X1.js
@@ -2,7 +2,7 @@ export class X1 {
     static sum_of_squares(lower_bound, upper_bound) {
         let sum = 0;
 
-        for (let i = lower_bound; i <= upper_bound; i++) {
+        for (const i of range(lower_bound, upper_bound)) {
             sum += Math.pow(i, 2)
         }
 
