d9f2c13cc7b0df02b75bc1bcbb22ab423c9f117b
Jez Higgins <jez@jezuk.co.uk>
2026-06-20 10:35:49 +0100

    It works!

--- divergent_change/src/CustomerService.js
+++ divergent_change/src/CustomerService.js
@@ -1,11 +1,17 @@
-export class CustomerService {
-    isValidEmail(email) {
+export class EmailValidator {
+    static isValid(email) {
         if (email === null) {
             return false;
         }
 
         const emailRegex = /^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$/;
         return emailRegex.test(email);
+    }
+}
+
+export class CustomerService {
+    isValidEmail(email) {
+        return EmailValidator.isValid(email);
     }
 
     formatDisplayName(firstName, lastName) {
