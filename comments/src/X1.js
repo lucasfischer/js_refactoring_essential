ffc23d3e4b230122f1914651a7b6f25a0c8eebe0
Jez Higgins <jez@jezuk.co.uk>
2026-06-20 10:09:44 +0100

    It works!

--- comments/src/X1.js
+++ comments/src/X1.js
@@ -5,14 +5,14 @@ export class X1 {
         // Iterate from lower bound (q) to upper bound (z)
         for (let i = q; i <= z; i++) {
             // Add square of each number in the range
-            p += X1.n(i);
+            p += X1.square(i);
         }
 
         // Return accumulated sum
         return p;
     }
 
-    static n(k) {
+    static square(k) {
         // Return square of input
         return k * k;
     }
